4c6c710138ec9dfa4f672a6d5a0e3bb1d647328c
Gabriel Pinheiro <gabrielp.siqueira2000@hotmail.com>
2024-10-09 20:49:25 -0300

    feat(p1-pdmn): cria componente principal e faz hello

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div className='container p-4 mt-2'>
+        <div className='row justify-content-center'>
+            <div className='col-sm-12 col-md-8 text-center'>
+                <h1>Hello, tarefas</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
